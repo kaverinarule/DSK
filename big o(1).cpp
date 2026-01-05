f80ad713fb9f55f5af4271fe22c45ad3d58e5914
kaverinarule <kaverinarule@gmail.com>
2026-01-05 22:12:29 +0530

    Add files via upload

---
+++ big o(1).cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 100;  
+    cout << "First element: " << n << endl; 
+    return 0;
+}
---
+++ bigo (n).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        cout << "Iteration " << i+1 << endl;
+    }
+    return 0;
+}
